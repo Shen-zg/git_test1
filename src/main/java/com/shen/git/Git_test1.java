ac278e685423077191e1bea33047ea23794b60c8
Shenzhigang <Shenzhigang@qq.com>
2022-01-12 15:33:36 +0800

    hot-fix commit1

--- src/main/java/com/shen/git/Git_test1.java
+++ src/main/java/com/shen/git/Git_test1.java
@@ -4,5 +4,6 @@ public class Git_test1 {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
