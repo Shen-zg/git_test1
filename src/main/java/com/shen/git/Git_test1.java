24cafa241190da4f3ddc2e27777bd7b807dc7973
Shenzhigang <Shenzhigang@qq.com>
2022-01-12 15:04:38 +0800

    second commit

--- src/main/java/com/shen/git/Git_test1.java
+++ src/main/java/com/shen/git/Git_test1.java
@@ -3,5 +3,6 @@ package com.shen.git;
 public class Git_test1 {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
